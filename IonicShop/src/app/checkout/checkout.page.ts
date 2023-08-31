82cdd8a2be5fa2bb4f984e9f3ad315fd1c4302f0
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2023-08-30 21:11:57 -0300

    29/08
    itens funcionando certo, agora e fazer o design

--- IonicShop/src/app/checkout/checkout.page.ts
+++ IonicShop/src/app/checkout/checkout.page.ts
@@ -1,32 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from '../services/cart.service';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-checkout',
-  templateUrl: 'checkout.page.html',
-  styleUrls: ['checkout.page.scss'],
+  templateUrl: './checkout.page.html',
+  styleUrls: ['./checkout.page.scss'],
 })
 export class CheckoutPage {
-  constructor(private cartService: CartService, private sanitizer: DomSanitizer) {}
+  constructor(public cartService: CartService, private router: Router) {}
 
-  getCartItems() {
-    return this.cartService.getCartItems();
-  }
-
-  getWhatsAppMessage(): SafeUrl {
-    let message = 'Pedido de Produtos:\n';
+  goToCheckout() {
     const cartItems = this.cartService.getCartItems();
-  
+    let message = 'Pedido de Produtos:\n';
+
     for (const item of cartItems) {
       message += item.name + '\n';
     }
-  
+
     const encodedMessage = encodeURIComponent(message);
-    const phoneNumber = '5596984008141'; // Seu número de telefone
-    const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
-    return this.sanitizer.bypassSecurityTrustUrl(whatsappLink);
+    const phoneNumber = '96984008141'; // Seu número de telefone aqui
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappUrl, '_blank');
   }
-  
-  
 }
